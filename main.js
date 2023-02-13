8579a0894d7d258b4c52686e3998e573e298f87e
mani-2012 <106404786+mani-2012@users.noreply.github.com>
2023-02-12 20:59:21 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ function setup() {
     cuadro=createCanvas(540,370);
     cuadro.position(480,160);
 }
+function preload() {
+    coco=loadImage("mani.jpg");
+}
+function draw() {
+    image(coco,0,0,540,370);
+    fill("blue");
+    text("Mani",360,137);
+    noFill();
+    stroke("burlywood");
+    strokeWeight(07);
+    rect(360,160,120,130);
+}
